5f381a45e17b8950d43fc7a1c94f640ab44dd386
Tom Sommer <tomsommer@users.noreply.github.com>
2015-07-23 11:25:11 +0200

    Check for SERVER_PORT or fall back to raw browser detection

--- aaa-ishyoboy-google-chrome-44-ssl-fix.php
+++ aaa-ishyoboy-google-chrome-44-ssl-fix.php
@@ -17,7 +17,9 @@ if ( ! class_exists( 'Ishyoboy_Google_Chrome_44_SSL_Fix' ) ) :
 
 		function __construct() {
 
-			if ( '80' == $_SERVER['SERVER_PORT'] ) {
+			if ( isset($_SERVER['SERVER_PORT']) && '80' == $_SERVER['SERVER_PORT'] || 
+				( !isset($_SERVER['SERVER_PORT']) && (false !== strstr($_SERVER['HTTP_USER_AGENT'], 'Chrome/44') || false !== strstr($_SERVER['HTTP_USER_AGENT'], 'Chrome/45')) ) 
+				) {
 				unset($_SERVER['HTTPS']);
 				unset($_SERVER['HTTP_HTTPS']);
 			}
